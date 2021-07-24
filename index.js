ec8a854f7848fc17a51c94dc116d0b31e3f1bc17
sebarray <sebarray@gmail.com>
2021-07-24 02:50:07 -0300

    s

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
+var nombre = prompt("cual es tu nombre")
 const ws= new WebSocket('wss://ws--go.herokuapp.com/ws')
+let text = document.getElementById('text');
 ws.onopen=()=>{
     console.log("conecctado")
 }
@@ -9,12 +10,22 @@ ws.onerror=(e)=>{
 ws.onmessage=(e)=>{
     console.log(e.data);
     h=document.createElement('div')
-    h.innerHTML=` <p class="text-white"> ${e.data}</p> `
-    document.getElementById('b').appendChild(h)
+    h.innerHTML=` <p class="text-white text-center fs-1">${nombre}: ${e.data}</p> `
+    document.getElementById('b').append(h)
+  
 }
+
+
  enviar.addEventListener('click',(e)=>{
      e.preventDefault();
     t= document.getElementById('text').value
     ws.send(JSON.stringify(t))
   
 })
+
+text.addEventListener("keypress", (e)=>{
+   if(e.code==="Enter"){
+    t= document.getElementById('text').value
+    ws.send(JSON.stringify(t)) 
+   }
+}, true)
